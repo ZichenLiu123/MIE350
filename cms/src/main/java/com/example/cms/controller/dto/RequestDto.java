a8ead267700bed96959e397414755a37c9305e60
fahds-eden <fahdfares7@yahoo.com>
2025-03-25 19:48:24 -0400

    Request Dto

---
+++ cms/src/main/java/com/example/cms/controller/dto/RequestDto.java
@@ -0,0 +1,12 @@
+package com.example.cms.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RequestDto {
+    private String alcoholType;
+    private String flavor;
+    private double price; 
+}
